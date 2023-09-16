3e73ec28d6d8d6b16fb4ec970621f5e00bb816f7
Charels Bruno <charlesbrcosta@gmail.com>
2023-09-15 23:28:35 -0300

    connecting front and back-end

--- server/download.js
+++ server/download.js
@@ -1,20 +1,24 @@
 import ytdl from 'ytdl-core'
 import fs from 'fs'
+// import { resolve } from 'path'
+// import { reject} from 'path'
 
-export const download = (videoId) => {
+export const download = (videoId) => new Promise((resolve, reject) => {
     const videoURL = "https://www.youtube.com/shorts/" + videoId
     console.log("Realizando Download do vídeo:", videoId)
 
-    ytdl(videoId, { quality: "lowestaudio", filter: "audioonly" })
-    .on("info", (info) =>{
+    ytdl(videoURL, { quality: "lowestaudio", filter: "audioonly" }).on("info", (info) => {
         const seconds = info.formats[0].approxDurationMs / 1000
         
         if(seconds > 60){
             throw new Error("A duração desse vídeo é maior do que 60 segundos.")
         }
+
     }).on("end", () => {
-        console.log("Download o vídeo finalizado...");
-    }).on("error", (error) =>{
-        console.log("Não foi possível fazer o download do vídeo. Detalhes do erro: ", error)
+        console.log("Download do vídeo finalizado.")
+        resolve()
+    }).on("error", (error) => {
+        console.log("Não foi possível fazer o download do vídeo. Detalhes do erro:", error)
+        reject(error)
     }).pipe(fs.createWriteStream("./tmp/audio.mp4"))
-}
+})
--- server/index.js
+++ server/index.js
@@ -1,16 +1,25 @@
 import cors from 'cors'
 import express from 'express'
+
 import { download } from './download.js'
+import { transcribe } from './transcribe.js'
+import { summarize } from "./summarize.js"
 
 const app = express()
+app.use(express.json())
 app.use(cors())
 
-app.get('/summary/:id', (request, response) => {
-    download(request.params.id)
-   // response.send("Id do vídeo: " + request.params.id)
-   response.json({ result: "Download do vídeo realizado com sucesso!" })
+app.get("/summary/:id",  async (request, response) => {
+    await download(request.params.id)
+    const result = await transcribe()
+ 
+    return response.json({ result })
 })
 
-var port = 3333 
-
-app.listen(port, () => console.log("Server is running on port", port))
+app.post("/summary", async (request, response) => {
+    const result = await summarize(request.body.text)
+    
+    return response.json({ result })
+})
+//const port = 3333 
+app.listen(3333, () => console.log("Server is running on port 3333"))
